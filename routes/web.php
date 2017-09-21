a7fbab4e17e8e354d8eeabdcb76d5efee82b46a5
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2017-09-21 13:21:23 +0000

    Got the stylesheet to work

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 Auth::routes();
 
+// Threads
 Route::get('/threads', 'ThreadsController@index');
 Route::get('/threads/create', 'ThreadsController@create');
 Route::get('/threads/{id}', 'ThreadsController@show');
+
+// Profile
+Route::get('/profile', 'ProfileController@index');
